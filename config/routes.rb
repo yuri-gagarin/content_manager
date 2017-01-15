dc9fddbd693c0e00a0cd08e7902392c9e9bf36cd
Yuriy Ivanov <ymivanov@icloud.com>
2017-01-15 01:38:48 -0500

    Created Subjects Index and Show

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
-  get 'demo/index'
+  
+  root 'demo#index'
+
+  resources :subjects do
+  	member do
+  	  get :delete
+  	end
+  end
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
