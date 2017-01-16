0d855278a62759e6da3dbdb56b5bbc32cb249a45
Yuriy Ivanov <ymivanov@icloud.com>
2017-01-15 19:22:00 -0500

    CRUD for Subjects complete

--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -9,24 +9,69 @@ class SubjectsController < ApplicationController
   def show
 
     @subject = Subject.find(params[:id])
-    
+
   end
 
   def new
+
+    @subject = Subject.new({name: 'Name Here', position: 'Position Here'})
+
   end
 
   def create
+
+    @subject = Subject.new(subject_params)
+
+    if @subject.save
+      #redirect to index if successful
+      redirect_to(subjects_path)
+    else
+      render(:new)
+    end
+
   end
 
   def edit
+
+    @subject = Subject.find(params[:id])
+
   end
 
   def update
+
+    @subject = Subject.find(params[:id])
+
+    if @subject.update_attributes(subject_params)
+      redirect_to(subject_path(@subject))
+    else
+      render(:edit)
+    end
+
   end
 
   def delete
+
+    @subject = Subject.find(params[:id])
+
   end
 
   def destroy
+
+    @subject = Subject.find(params[:id])
+
+    if @subject.destroy
+      redirect_to(subjects_path)
+    else
+      render(:delete)
+    end
+
+  end
+
+  private
+
+  def subject_params
+
+    params.require(:subject).permit(:name, :position, :visible)
+
   end
 end
